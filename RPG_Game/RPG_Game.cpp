dbd9c07d884831dec651eee52a9a881eb830043e
Kostya <kostya123qwertyzubenko4.0@gmail.com>
2024-09-29 17:17:16 +0700

    Обновлены структуры персонажа и сущности

--- RPG_Game/RPG_Game.cpp
+++ RPG_Game/RPG_Game.cpp
@@ -4,24 +4,25 @@
 #include <string.h>
 #include <locale.h>
 
+//Структура персонажа
 typedef struct {
     char name[50]; //Имя персонажа
     int health; //Значение здоровья персонажа
     int level; //Уровень персонажа
     int attack_power; //Значение силы персонажа
-    struct Location* current_location; //Текущее местоположение персонажа
-    struct Item* inventory[20];//Массив, в котором хранятся вещи персонажа
-} Player;//Название структуры
+} Player;
 
+//Структура существа
 typedef struct {
     char name[50];//Имя сущности
     int health;//Значение здоровья сущности
-    int level;//Уровень сущности
     int attack_power;//Значение силы сущности
-} Enemy;//Название структуры
+} Enemy;
 
 int main()
 {
 	setlocale(LC_ALL, "RU");
 
+    
+    return 0;
 }
