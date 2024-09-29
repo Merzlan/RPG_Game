4e118b8860ee02f84ff26d0c6c8182186107642d
Kostya <kostya123qwertyzubenko4.0@gmail.com>
2024-09-29 17:37:46 +0700

    Добавлены структуры данных для предметов, локаций, квестов, инвентаря, событий и способности

--- RPG_Game/RPG_Game.cpp
+++ RPG_Game/RPG_Game.cpp
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -18,6 +20,47 @@ typedef struct {
     int health;//Значение здоровья сущности
     int attack_power;//Значение силы сущности
 } Enemy;
+
+//Структура предмета
+typedef struct {
+    char name[50];//Название предмета
+    int value;//Цена предмета
+    int type; // 0 - Оружие, 1 - Снаряжение
+} Item;
+
+//Структура локации
+typedef struct {
+    char description[255];//Описание локации
+    Item items[10];//Массив предметов в локации
+    int itemCount;//Количество предметов
+} Location;
+
+//Структура квеста
+typedef struct {
+    char title[50];//Название квеста
+    char description[255];//Описание квеста
+    int isCompleted;//Статус завершения
+} Quest;
+
+//Структура инвентаря
+typedef struct {
+    Item items[20];//Массив предметов в инвентаре
+    int itemCount;//Количество предметов в инвентаре
+} Inventory;
+
+//Структура события
+typedef struct {
+    char description[100];//Описание события
+    int isTriggered;//Статус срабатывания
+} Event;
+
+//Структура способности
+typedef struct {
+    char name[50];//Название способности
+    int power;//Сила способности
+    int cooldown;//Перезарядка способности
+} Ability;
+
 
 int main()
 {
